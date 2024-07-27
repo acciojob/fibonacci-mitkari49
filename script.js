f980c7797b5b646615c367486d0f4be93c7bc013
mitkari49 <93242478+mitkari49@users.noreply.github.com>
2024-07-27 21:39:53 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,18 @@
 function fibonacci(num) {
-// your code here
+  if (num <= 0) {
+    return 0;
+  } else if (num === 1) {
+    return 1;
+  } else {
+    let a = 0;
+    let b = 1;
+    for (let i = 2; i <= num; i++) {
+      let temp = a + b;
+      a = b;
+      b = temp;
+    }
+    return b;
+  }
 }
 
-module.exports = fibonacci;
+module.exports = fibonacci(3);
