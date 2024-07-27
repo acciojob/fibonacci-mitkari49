6f37d0efdea2e2d188caef1ab929a4e6df02959d
mitkari49 <93242478+mitkari49@users.noreply.github.com>
2024-07-27 21:51:28 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,12 +1,14 @@
 function fibonacci(num) {
   if (num <= 0) {
-    return 0;
+    return 0;  // Although the problem states the input will be positive, this handles the case num = 0.
   } else if (num === 1) {
+    return 0;
+  } else if (num === 2) {
     return 1;
   } else {
     let a = 0;
     let b = 1;
-    for (let i = 2; i <= num; i++) {
+    for (let i = 3; i <= num; i++) {
       let temp = a + b;
       a = b;
       b = temp;
